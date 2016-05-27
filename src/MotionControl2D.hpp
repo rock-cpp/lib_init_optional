53607495692a9d166eac61497371da942c2c3d75
Janosch Machowinski <Janosch.Machowinski@dfki.de>
2016-05-27 18:10:07 +0200

    Introduced new interface MotionControl2D

---
+++ src/MotionControl2D.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <orocos_cpp_base/ProxyPort.hpp>
+#include <base/samples/CommandSamples.hpp>
+#include <lib_init/Base.hpp>
+
+namespace init
+{
+
+class MotionControl2D : public Base
+{
+public:
+    MotionControl2D(const std::string& name): Base(name) {};
+    virtual ~MotionControl2D() {};
+    virtual InputProxyPort<base::commands::Motion2D> &getCommand2DPort() = 0;
+};
+}
