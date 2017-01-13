46b057947d3084cc0492820459693a3f7f6e4ca0
Sascha Arnold <sascha.arnold@dfki.de>
2017-01-13 17:47:34 +0100

    added MLS providers for the sloped and precalculated MLS

--- src/optional/MLSProvider.hpp
+++ src/optional/MLSProvider.hpp
@@ -11,11 +11,35 @@ namespace init
 class MLSProvider : public virtual Base
 {
 public:
-    MLSProvider(const std::string& name);
-    virtual ~MLSProvider();
+    MLSProvider(const std::string& name) : Base(name) {}
+    virtual ~MLSProvider() {}
     
     virtual OutputProxyPort<envire::core::SpatioTemporal<maps::grid::MLSMapKalman > >& getMapPort() = 0;
     
+    virtual bool generateMap() = 0;
+
+};
+
+class MLSSlopedProvider : public virtual Base
+{
+public:
+    MLSSlopedProvider(const std::string& name) : Base(name) {}
+    virtual ~MLSSlopedProvider() {}
+
+    virtual OutputProxyPort<envire::core::SpatioTemporal<maps::grid::MLSMapSloped > >& getMapPort() = 0;
+
+    virtual bool generateMap() = 0;
+
+};
+
+class MLSPrecalculatedProvider : public virtual Base
+{
+public:
+    MLSPrecalculatedProvider(const std::string& name) : Base(name) {}
+    virtual ~MLSPrecalculatedProvider() {}
+
+    virtual OutputProxyPort<envire::core::SpatioTemporal<maps::grid::MLSMapPrecalculated > >& getMapPort() = 0;
+
     virtual bool generateMap() = 0;
 
 };
