d9d8527fc2a2a1033525236e13f2b9396084404f
Janosch Machowinski <janosch.machowinski@dfki.de>
2017-10-13 13:35:03 +0200

    fixed RealtimeUrdfFilter

--- src/optional/RealtimeUrdfFilter.cpp
+++ src/optional/RealtimeUrdfFilter.cpp
@@ -1,11 +1,40 @@
 #include "RealtimeUrdfFilter.hpp"
 
-init::RealtimeUrdfFilter::RealtimeUrdfFilter(init::DistanceImageProvider& distImage, const std::string& taskName): 
+init::RealtimeUrdfFilter::RealtimeUrdfFilter(const std::string& taskName, init::DistanceImageProvider& distImage, init::RobotFrames &frame): 
     DistanceImageProvider("RealtimeUrdfFilter"),
+    frame(frame),
+    distImage(distImage),
     filterTask(this, taskName)
 {
     registerDependency(distImage);
+    registerDependency(frame);
 }
+
+bool init::RealtimeUrdfFilter::connect()
+{
+    distImage.getDistanceImagePort().connectTo(filterTask.getConcreteProxy()->input_depth, RTT::ConnPolicy::buffer(10));
+    return init::Base::connect();
+}
+
+bool init::RealtimeUrdfFilter::start()
+{
+    //this is hacky and ugly, but the task does not honor any convetions, so....
+    frame.robotFrames.getConcreteProxy()->synchronize();
+    
+    RTT::DataFlowInterface *fi = frame.robotFrames.getConcreteProxy()->ports();
+    
+    for(RTT::base::PortInterface *pi : fi->getPorts())
+    {
+        std::string tn = pi->getTypeInfo()->getTypeName();
+        if(tn == "/base/samples/RigidBodyState")
+        {
+            pi->connectTo(filterTask.getConcreteProxy()->input_tf.getPortInterface(), RTT::ConnPolicy::buffer(100));
+        }
+    }
+    
+    return init::Base::start();
+}
+
 
 OutputProxyPort< base::samples::DistanceImage >& init::RealtimeUrdfFilter::getDistanceImagePort()
 {
--- src/optional/RealtimeUrdfFilter.hpp
+++ src/optional/RealtimeUrdfFilter.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
 #include <lib_init/DistanceImageProvider.hpp>
+#include <lib_init/RobotFrames.hpp>
 #include <realtime_urdf_filter/proxies/RealtimeURDFFilterTask.hpp>
 
 namespace init {
 
 class RealtimeUrdfFilter : public init::DistanceImageProvider
 {
+    init::RobotFrames &frame;
+    init::DistanceImageProvider &distImage;
 public:
-    RealtimeUrdfFilter(init::DistanceImageProvider &distImage, const std::string &taskName);
+    RealtimeUrdfFilter(const std::string &taskName, init::DistanceImageProvider &distImage, init::RobotFrames &frames);
 
+    virtual bool connect();
+    virtual bool start();
+    
     virtual OutputProxyPort< base::samples::DistanceImage >& getDistanceImagePort();
     
     DependentTask<realtime_urdf_filter::proxies::RealtimeURDFFilterTask> filterTask;
